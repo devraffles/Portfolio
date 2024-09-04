ee97e0794815455041304b674d283a9d5133204b
Rafael dos Santos Giorgi <106035150+devraffles@users.noreply.github.com>
2024-09-04 14:15:10 -0300

    v1-portifolio

---
+++ script/script.js
@@ -0,0 +1,51 @@
+gsap.registerPlugin(ScrollTrigger);
+
+AOS.init();
+
+const pageContainer = document.querySelector(".container-scroll")
+
+const scroller = new LocomotiveScroll({
+    el: pageContainer,
+    smooth: true
+})
+
+scroller.on("scroll", ScrollTrigger.update)
+
+ScrollTrigger.scrollerProxy(pageContainer,  {
+    scrollTop(value){
+        return arguments.length
+        ? scroller.scrollTo(value, 0, 0)
+        : scroller.scroll.instance.scroll.y
+    },
+    getBoundingClientRect(){
+        return {
+            left: 0,
+            top: 0,
+            width: window.innerWidth,
+            height: window.innerHeight,
+        }
+    },
+    pinType: pageContainer.style.transform ? "transform" : "fixed"
+})
+
+window.addEventListener("load", function(){
+    let pinWrap = document.querySelector(".pin-wrap");
+    let pinWrapWidth = pinWrap.offsetWidth;
+    let horizontalScrollLength = pinWrapWidth - window.innerWidth;
+
+    gsap.to(".pin-wrap", {
+        scrollTrigger:{
+            scroller: pageContainer,
+            scrub: true,
+            trigger: "#sectionPin",
+            pin: true,
+            start: "top top",
+            end: pinWrapWidth,
+        },
+        x: -horizontalScrollLength
+    })
+
+    ScrollTrigger.addEventListener("refresh", () => scroller.update())
+
+    ScrollTrigger.refresh();
+})
